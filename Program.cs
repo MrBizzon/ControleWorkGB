2eb1bd537aaa0419c5e2c109bff1f781bb27ecdd
MrBizzon <oleg.zubarev@mail.ru>
2022-09-17 16:14:46 +0300

    Added a solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreatArray;
+int number;
+
+Console.Write("Enter the size of the array: ");
+number = Convert.ToInt32(Console.ReadLine());
+CreatArray = new string[number];
+Console.WriteLine("Enter any numbers, words, or symbols: ");
+
+for (int i = 0; i < CreatArray.Length; i++)
+{
+    Console.Write($"Input {i + 1}:  ");
+    CreatArray[i] = Console.ReadLine();
+}
+
+string str = string.Join(", ", CreatArray);
+Console.Write("The condition is met for the following lines:  ");
+for (int i = 0; i < CreatArray.Length; i++)
+{
+    if (CreatArray[i].Length <= 3)
+    {
+        Console.Write("{0} ", string.Join(",", CreatArray[i]));
+    }
+}
